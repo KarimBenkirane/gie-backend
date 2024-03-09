52564be437efa71bbe8476222c2b1a5e66a4ad57
khalil <khalilaraoui1@email.com>
2024-03-09 17:36:37 +0100

    first commit

--- src/main/java/ma/uiass/eia/controller/App.java
+++ src/main/java/ma/uiass/eia/controller/App.java
@@ -1,7 +1,7 @@
 package ma.uiass.eia.controller;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World !");
+    public static void main(String [] args) {
+       
     }
 }
